eeeb7571d9d560bb536f86edc8301955d6d66d78
Caleb McAnuff <cjmcanuff@yahoo.ca>
2023-12-12 11:05:23 -0500

    silenced gui in installl.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 sudo apt-get update -y
-sudo apt-get upgrade -y
+
+# silence gui output
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
 sudo apt install python3-pip
 sudo apt install awscli
+sudo apt-get install git-lfs
+
+# python requirements
 pip install -r requirements.txt
 pip install tensorflow
 pip install --upgrade flask jinja2 markupsafe
-# sudo apt install unzip
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
+
+# add usr/local to PATH
+echo "export PATH=$PATH:/home/ubuntu/.local/bin" >> ~/.bashrc
+source ~/.bashrc
 
 
