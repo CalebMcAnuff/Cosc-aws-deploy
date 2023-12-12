47a249d543686cf0a99e57054cf7b3b30723e8ac
Caleb McAnuff <cjmcanuff@yahoo.ca>
2023-12-11 22:56:43 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install python3-pip
-pip install -r requirements.txt
 sudo apt install awscli
-
+pip install -r requirements.txt
+pip install --upgrade flask jinja2 markupsafe
 # sudo apt install unzip
 # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 # unzip awscliv2.zip
