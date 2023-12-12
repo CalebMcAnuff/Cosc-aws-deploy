7ff44c4656f09e360ccbb5168d1b9a75d2ec5f58
Caleb McAnuff <cjmcanuff@yahoo.ca>
2023-12-11 23:50:10 -0500

    updated readme

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt-get upgrade -y
 sudo apt install python3-pip
 sudo apt install awscli
 pip install -r requirements.txt
+pip install tensorflow
 pip install --upgrade flask jinja2 markupsafe
 # sudo apt install unzip
 # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
