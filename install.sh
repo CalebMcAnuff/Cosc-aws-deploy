9d53edc920646ec887052956e5ca1935a990e177
Caleb McAnuff <cjmcanuff@yahoo.ca>
2023-12-11 20:30:53 -0500

    added install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install python3-pip
+pip install -r requirements.txt
+sudo apt install awscli
+
+# sudo apt install unzip
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+
+
